328f761f3aa9fa282693a6aa6996f62e841014fd
Your Name <you@example.com>
2021-09-15 16:02:38 +0430

    add android moves suppurt

--- App.js
+++ App.js
@@ -163,6 +163,7 @@ function moveHandler(moveMethod) {
   fillBoxes(initialNumbers);
 }
 
+// windows events
 document.onkeydown = (e) => {
   switch (e.key) {
     case "ArrowLeft": {
@@ -185,6 +186,56 @@ document.onkeydown = (e) => {
       break;
   }
 };
+
+// mobile events
+document.addEventListener("touchstart", handleTouchStart, false);
+document.addEventListener("touchmove", handleTouchMove, false);
+
+var xDown = null;
+var yDown = null;
+
+function getTouches(evt) {
+  return (
+    evt.touches || // browser API
+    evt.originalEvent.touches
+  ); // jQuery
+}
+
+function handleTouchStart(evt) {
+  const firstTouch = getTouches(evt)[0];
+  xDown = firstTouch.clientX;
+  yDown = firstTouch.clientY;
+}
+
+function handleTouchMove(evt) {
+  if (!xDown || !yDown) {
+    return;
+  }
+
+  var xUp = evt.touches[0].clientX;
+  var yUp = evt.touches[0].clientY;
+
+  var xDiff = xDown - xUp;
+  var yDiff = yDown - yUp;
+
+  if (Math.abs(xDiff) > Math.abs(yDiff)) {
+    /*most significant*/
+    if (xDiff > 0) {
+      moveHandler(moveArrayToLeft);
+    } else {
+      moveHandler(moveArrayToRight);
+    }
+  } else {
+    if (yDiff > 0) {
+      moveHandler(moveArrayToTop);
+    } else {
+      moveHandler(moveArrayToBottom);
+    }
+  }
+  /* reset values */
+  xDown = null;
+  yDown = null;
+}
 
 //-----------------------------------------
 // on game load
